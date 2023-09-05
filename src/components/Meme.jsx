49a04a5aa23ff63f8a1a79f447a8cedf806037f4
richasudikonda <vermaricha829@gmail.com>
2023-09-05 05:44:06 -0400

    Get Random meme images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import memesData from "../memesData"
 
 export default function Meme(){
-    return(
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+
+    return (
         <main>
-            <form className="form">
-                <input type="text" placeholder="Shut up" className="form--input"/>
-                <input type="text" placeholder="and take my money" className="form--input"/>
-                <button className="form--button">Get a new meme image</button>
-            </form>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
+
+
+
+/**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
